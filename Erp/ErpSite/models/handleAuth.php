bb7f7e61b453cbd86c3297ff972f29d3a222be9a
soham <soham@patkar.com>
2020-10-26 18:46:42 +0530

    directory changes

--- Erp/ErpSite/models/handleAuth.php
+++ Erp/ErpSite/models/handleAuth.php
@@ -5,33 +5,22 @@ require 'connDB.php';
 function get_user($username, $password) {
     global $conn;
     try {
-        $sql = "SELECT * FROM `users` where `username` = '$username' and `password` = '$password' LIMIT 1 ";
+        $sql = "SELECT * FROM `user` where `username` = '$username' and `password` = '$password' LIMIT 1 ";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_row()) {
                 $currentUser = $row[0];
-                $userGroup = get_group($username);
+                $sql1 = "SELECT * FROM `groups` where `id` = $row[4] LIMIT 1";
+                $result1 = $conn->query($sql1);
+                $row1 = $result1->fetch_row();
+                $userGroup = $row1[0];
                 return array($currentUser, $userGroup);
             };
         } else {
             return false;
         }
     }
-    catch(Exception $e) {
-        echo "ERROR ! \n cause of error: "  .$e->getMessage();
-    }
-}
-
-function get_group($username) {
-    global $conn;
-    try {
-        $sql = "SELECT * FROM usergroups where username = '$username' LIMIT 1 ";
-        $result = $conn->query($sql);
-        while ($row = $result->fetch_row()) {
-            return $row[2];
-        };
-}
     catch(Exception $e) {
         echo "ERROR ! \n cause of error: "  .$e->getMessage();
     }
