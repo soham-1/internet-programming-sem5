9bfaa4bb9f45eef8ebdbf9821f25a306ba496713
Hemant <yadavhemant1716@gmail.com>
2020-10-27 13:25:00 +0530

    register , login done

--- Erp/ErpSite/views/signup.php
+++ Erp/ErpSite/views/signup.php
@@ -1,84 +1,450 @@
 <?php
+$showalert = false;
+$showerror = false;
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-$dir = explode('htdocs\\', dirname(dirname(__FILE__)), 2)[1];
-$basedir = explode('htdocs\\', dirname($dir))[0];
+include '../models/connDB.php';
 
-if ($_POST) {
-    // require '../models/register.php';
-    require '../models/connDB.php';
+$username = $_POST['username'];
+$password = $_POST['password'];
+$cpassword = $_POST['cpassword'];
+$email = $_POST['email'];
+$group = $_POST['group'];
+// $exists = false;
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $group = $_POST['group'];
-    function register($username, $password, $email, $group) {
-        global $conn;
-        try {
-            $groupsql = "SELECT `id` FROM `groups` where 1";
-            // $result = mysqli_query($conn, $groupsql);
-            $result = $conn->query($groupsql);
-            $row = $result->fetch_row();
-            $usersql = "INSERT INTO `user`(`username`, `password`, `email`, `groups`) VALUES ('$username', '$password', '$email', 1)";
-            // $usersql = "SELECT `id` FROM `groups` where `group_name` = `$group`";
-            $result = $conn->query($usersql);          
-            return array(200, "account created successfully");
-        } catch(Execption $e) {
-            return array(500, $e->getMessage());
-        }
-    }
-    $status = register($username, $password, $email, $group);
-
-
-
-    if ($status[0]==200) {
-        switch($group) {
-            case 'admin':
-                {
-                    $url = $dir . "/templates/adminHome.html";
-                    header('Location: /'.$url);
-                    break;
-                }
-            case 'customer':
-                {
-                    $url = $basedir . "/Retailer/templates/retailHome.html";
-                    header('Location: /'.$url);
-                    break;
-                }
-            case 'shop':
-                {
-                    $url = $basedir . "/Enduser/templates/enduserHome.html";
-                    header('Location: /'.$url);
-                    break;
-                }
-        }
-    } else if ($status[0]==500) {
-        echo $status[1];
+$existSql = "Select * from `user` where username='$username'";
+$result = mysqli_query($conn, $existSql);
+$numExistRows = mysqli_num_rows($result);
+if($numExistRows > 0){
+  // $exists = true;9*
+  $showerror ="Username already exists.";
+}
+else{
+  // $exists = false;
+  if ($password == $cpassword ){
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO `user` ( `username`, `password`, `email`, `groups`) VALUES ('$username', '$hash',  '$email', '$group')";
+    $result = mysqli_query($conn,$sql);
+    if ($result){
+            $showalert = true;
     }
 }
-
+  else{
+    $showerror = "Passwords do not match";
+}
+}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>ERP</title>
 </head>
+<style>
+
+html {
+  height: 100%;
+}
+body {
+  margin:0;
+  padding:0;
+  font-family: sans-serif;
+  background: linear-gradient(#141e30, #243b55);
+}
+
+.login-box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  background: rgba(0,0,0,.5);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+  border-radius: 10px;
+
+}
+/* .login-box label{
+    color:white;
+
+} */
+
+.login-box h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
+
+.login-box .user-box {
+  position: relative;
+}
+
+.login-box .user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 20px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+.login-box .user-box label {
+  position: absolute;
+  top:0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: .5s;
+}
+
+.login-box .user-box input:focus ~ label,
+.login-box .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #03e9f4;
+  font-size: 12px;
+}
+
+/* .login-box form a {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #03e9f4;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: .5s;
+  margin-top: 40px;
+  letter-spacing: 4px
+}
+
+.login-box a:hover {
+  background: #03e9f4;
+  color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #03e9f4,
+              0 0 25px #03e9f4,
+              0 0 50px #03e9f4,
+              0 0 100px #03e9f4;
+}
+
+.login-box a span {
+  position: absolute;
+  display: block;
+} */
+
+/* .login-box a span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #03e9f4);
+  animation: btn-anim1 1s linear infinite;
+}
+
+@keyframes btn-anim1 {
+  0% {
+    left: -100%;
+  }
+  50%,100% {
+    left: 100%;
+  }
+}
+
+.login-box a span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #03e9f4);
+  animation: btn-anim2 1s linear infinite;
+  animation-delay: .25s
+}
+
+@keyframes btn-anim2 {
+  0% {
+    top: -100%;
+  }
+  50%,100% {
+    top: 100%;
+  }
+}
+
+.login-box a span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(270deg, transparent, #03e9f4);
+  animation: btn-anim3 1s linear infinite;
+  animation-delay: .5s
+}
+
+@keyframes btn-anim3 {
+  0% {
+    right: -100%;
+  }
+  50%,100% {
+    right: 100%;
+  }
+}
+
+.login-box a span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #03e9f4);
+  animation: btn-anim4 1s linear infinite;
+  animation-delay: .75s
+}
+
+@keyframes btn-anim4 {
+  0% {
+    bottom: -100%;
+  }
+  50%,100% {
+    bottom: 100%;
+  }
+} */
+/* .box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+} */
+.login-box form button {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #03e9f4;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: .5s;
+  margin-top: 40px;
+  letter-spacing: 4px
+}
+
+.login-box button:hover {
+  background: #03e9f4;
+  color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #03e9f4,
+              0 0 25px #03e9f4,
+              0 0 50px #03e9f4,
+              0 0 100px #03e9f4;
+}
+
+.login-box button span {
+  position: absolute;
+  display: block;
+}
+
+.login-box button span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #03e9f4);
+  animation: btn-anim1 1s linear infinite;
+}
+
+@keyframes btn-anim1 {
+  0% {
+    left: -100%;
+  }
+  50%,100% {
+    left: 100%;
+  }
+}
+
+.login-box button span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #03e9f4);
+  animation: btn-anim2 1s linear infinite;
+  animation-delay: .25s
+}
+
+@keyframes btn-anim2 {
+  0% {
+    top: -100%;
+  }
+  50%,100% {
+    top: 100%;
+  }
+}
+
+.login-box button span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(270deg, transparent, #03e9f4);
+  animation: btn-anim3 1s linear infinite;
+  animation-delay: .5s
+}
+
+@keyframes btn-anim3 {
+  0% {
+    right: -100%;
+  }
+  50%,100% {
+    right: 100%;
+  }
+}
+
+.login-box button span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #03e9f4);
+  animation: btn-anim4 1s linear infinite;
+  animation-delay: .75s
+}
+
+@keyframes btn-anim4 {
+  0% {
+    bottom: -100%;
+  }
+  50%,100% {
+    bottom: 100%;
+  }
+}
+.box select {
+  background-color: #0563af;
+  color: white;
+  padding: 12px;
+  width: 250px;
+  border: none;
+  font-size: 16px;
+  box-shadow: 0 5px 25px rgba(0, 0, 0, 0.2);
+  -webkit-appearance: button;
+  appearance: button;
+  outline: none;
+}
+
+.box::before {
+  font-family: FontAwesome;
+  /* position: absolute;
+  top: 0;
+  right: 0; */
+  width: 20%;
+  height: 100%;
+  text-align: center;
+  font-size: 16px;
+  line-height: 45px;
+  color: rgba(255, 255, 255, 0.5);
+  background-color: rgba(255, 255, 255, 0.1);
+  pointer-events: none;
+}
+
+/* .box:hover::before {
+  color: rgba(255, 255, 255, 0.6);
+  background-color: rgba(255, 255, 255, 0.2);
+} */
+
+.box select option {
+  padding: 30px;
+}
+
+
+/* @-webkit-keyframes glow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+  }
+
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
+  }
+} */
+    </style>
 <body>
-    <form action="./signup.php" method="post">
-        username
-        <input type="text" name="username" id="username">
-        password
-        <input type="text" name="password" id="password">
-        email
-        <input type="text" name="email" id="email">
-        group
-        <select name="group" id="group">
-            <option value="admin">admin</option>
-            <option value="shop">shop</option>
-            <option value="customer">customer</option>
-        </select>
-        <button type="submit">submit</button>
+<?php
+    require 'partials/_nav.php';
+    require '../models/connDB.php';
+    ?>
+
+    <?php
+   if($showalert){
+       echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> Your account has been created. Please <a href="login.php">Login here</a>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>';
+   }
+   if($showerror){
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+<strong>Error!</strong> '.$showerror.'
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+ <span aria-hidden="true">&times;</span>
+</button>
+</div>';
+}
+?>
+<div class="login-box">
+    <h2>Register</h2>
+    <form action="/php_projects/internet-programming-sem5/Erp/ErpSite/views/signup.php" method="POST">
+    <div class="user-box">
+        <input type="text" name="username" id="username" required>
+        <label>Username</label>
+        </div>
+        <div class="user-box">
+        <input type="password" name="password" id="password" required>
+        <label>Password</label>
+        </div>
+        <div class="user-box">
+        <input type="password" name="cpassword" id="cpassword" required>
+        <label>Confirm Password</label>
+        </div>
+        <div class="user-box">
+        <input type="email" name="email" id="email" required>
+        <label> Email</label>
+        </div>
+
+        <label style="color:white;">Group</label>
+       <?php  $sql = "SELECT * FROM groups";
+        $result = mysqli_query($conn,$sql);
+
+echo "</select>";
+echo " <div class='box'>
+        <select name='group' id='group'>";
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<option value='" . $row['id'] ."'>" . ucfirst($row['group_name'])."</option>";
+        }
+        echo "</select>";
+
+
+echo "</div>";
+?>
+        <button type=submit>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+</button>
     </form>
+
+</div>
 </body>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </html>
