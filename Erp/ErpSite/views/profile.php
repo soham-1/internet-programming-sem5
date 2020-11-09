30da2b14777e3648a2911168727065742b054d56
soham <soham@patkar.com>
2020-11-09 16:59:23 +0530

    ajax added

--- Erp/ErpSite/views/profile.php
+++ Erp/ErpSite/views/profile.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <?php require 'welcome.php'; ?>
+    <?php include('../models/connDB.php'); ?>
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/profile.css">
     <title>ERP</title>
-    <?php include('../models/connDB.php'); ?>
 </head>
 <?php 
-// include '../models/connDB.php';
-session_start();
 $email; $picture; $address; $phone_no; $group; $shop;
 
     function get_user_details() {
@@ -34,45 +34,65 @@ $email; $picture; $address; $phone_no; $group; $shop;
         
     }
 
+    if (count($_POST)>0) {
+        $sql = "UPDATE shop SET reg_no=".$_POST['reg-no'].", category=".$_POST['category']." WHERE shop_owner=" . $_SESSION['user_id'];
+        if ($conn->query($sql)==TRUE) {
+            $response = array(
+                "status_code" => 200,
+                "message" => "updates were made successfully !"
+            );
+            echo json_encode($response);
+            http_response_code(200);
+        }
+        else {
+            http_response_code(502);
+        }
+    }
+
 ?>
 <body>
     <?php get_user_details(); ?>
+
     <div class="container">
         <div class="side-section col-lg-3 col-md-6 col-sm-12">
-            <div class="profile-image">
-                <?php
-                    if($group[0]=='shop' && isset($shop[5]))
-                        echo '<img src="data:image/png;charset=utf8;base64,' . base64_encode($shop[5]) . '" class="rounded-img" />';
-                    elseif($group[0]=='customer' && isset($picture))
-                        echo '<img src="data:image/png;charset=utf8;base64,' . base64_encode($picture) . '" />';
-                ?>      
-            </div>
-            <div id="mapholder">map</div>
-            <div class="row">
-                <label for="email" class="detail-field values" id="email">email id</label>
-                <input type="text" class="detail-field values" id="email" value=<?php echo $email; ?>>
-            </div>
+            <form name="updateForm" action="profilp" method="post">
+                <div class="profile-image">
+                    <?php
+                        if($group[0]=='shop' && isset($shop[5]))
+                            echo '<img src="data:image/png;charset=utf8;base64,' . base64_encode($shop[5]) . '" class="rounded-img" />';
+                        elseif($group[0]=='customer' && isset($picture))
+                            echo '<img src="data:image/png;charset=utf8;base64,' . base64_encode($picture) . '" />';
+                    ?>      
+                </div>
+                <div id="mapholder">map</div>
+                <div class="row">
+                    <label for="email" class="detail-field values" id="email">email id</label>
+                    <input type="text" class="detail-field values" id="email" name="email" value=<?php echo $email; ?>>
+                </div>
+            <form name="updateForm" action="profilp" method="post">
         </div>
-        <div class="form-section col-lg-3 col-md-6 col-sm-12">
-            <div class="details">
-                <div class="header">
-                    <h2><?php echo $shop[2]; ?></h2>
+
+        <div class="form-section col-lg-7 col-md-6 col-sm-12">
+            <form name="updateForm" action="profilp" method="post">
+                <div class="details">
+                    <div class="header">
+                        <h2><?php echo $shop[2]; ?></h2>
+                    </div>
+                    <div class="row">
+                        <label for="owner" class="detail-field">shop owner</label>
+                        <input type="text" class="detail-field values" id="owner" name="owner" value=<?php echo $_SESSION['username']; ?>>
+                    </div>
+                    <div class="row">
+                        <label for="reg-no" class="detail-field">registration no.</label>
+                        <input type="text" class="detail-field values" id="reg-no" name="reg-no" value=<?php echo $shop[1]; ?>>
+                    </div>
+                    <div class="row">
+                        <label for="category" class="detail-field">category</label>
+                        <input type="text" class="detail-field values" id="category" name="category" value=<?php echo $shop[4]; ?>>
+                    </div>
                 </div>
-                <div class="row">
-                    <label for="owner" class="detail-field">shop owner</label>
-                    <input type="text" class="detail-field values" id="owner" value=<?php echo $_SESSION['username']; ?>>
-                    <button class="btn btn-primary">edit</button>
-                    <button class="btn btn-success">save</button>
-                </div>
-                <div class="row">
-                    <label for="reg-no" class="detail-field">registration no.</label>
-                    <input type="text" class="detail-field values" id="reg-no" value=<?php echo $shop[1]; ?>>
-                </div>
-                <div class="row">
-                    <label for="category" class="detail-field">category</label>
-                    <input type="text" class="detail-field values" id="category" value=<?php echo $shop[4]; ?>>
-                </div>
-            </div>
+                <button class="btn btn-success" id="form-btn" type="submit">save</button>
+            </form>
         </div>
     </div>
 
@@ -107,5 +127,29 @@ $email; $picture; $address; $phone_no; $group; $shop;
         //  }
         //  getLocation();
 
-      </script>
+        $(function () {
+
+            $('form').on('submit', function (e) {
+
+            e.preventDefault();
+
+            $.ajax({
+                type: 'post',
+                data: $('form').serialize(),
+                success: function (response) {
+                    alert(response);
+                    let res = $.parseJSON(response)
+                    if (res.status_code==200){
+                        alert('updates were made !');
+                    }
+                },
+                error: function (error) {
+                    alert("some error occured");
+                }
+            });
+
+            });
+
+        });
+</script>
 </html>
